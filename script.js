1bb33ff2b768c966bf49ca13ab7204dcf7cf7d7e
Eecodev <mnasser.bbk@gmail.com>
2023-10-24 16:46:51 +0200

    snack 1 completed, and scaffolding snack 2

--- script.js
+++ script.js
@@ -23,3 +23,75 @@ Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti supe
 Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120.
 */
 
+/*
+ *SNACK 1
+*/
+const tavoloVip = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin']
+
+let assegnazionePosti = tavoloVip.map((el, index)=>{
+    return {
+        Guest: el,
+        Tavolo: 'Vip',
+        Seat: index + 1
+    }
+})
+console.log(assegnazionePosti);
+
+// let result = []
+// tavoloVip.forEach((element, index)=>{
+//     result.push({
+//         Guest: element,
+        // Tavolo: 'Vip',
+//         seat: index + 1
+//     })
+// })
+// console.log(result);
+
+/*
+ *SNACK 2
+*/
+
+const studentsList = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grades: 78
+    },
+    {
+        id: 110,
+        name:'Paola Cortellessa',
+        grades: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    }
+]
+
+let listUpperCase = studentsList.forEach((element)=>{
+    return {
+        
+    }
+})
+console.log(listUpperCase);
