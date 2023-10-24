08b41743a295f7802eabef487766dca7292d3116
Eecodev <mnasser.bbk@gmail.com>
2023-10-24 15:52:07 +0200

    initial scaffolding

---
+++ utility.js
@@ -0,0 +1,19 @@
+function getRndInteger(min, max){
+    return Math.floor(Math.random()* (max - min + 1)) + min;
+}
+
+
+/**
+ * Determina se il numero è pari
+ * @param {Number} num 
+ * @returns {Boolean}
+ */
+function isEven(num){
+    // if(num % 2 === 0){
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    // let result = (num % 2 === 0) ? true : false;
+    result = (num % 2 === 0) ? true : false;
+}
